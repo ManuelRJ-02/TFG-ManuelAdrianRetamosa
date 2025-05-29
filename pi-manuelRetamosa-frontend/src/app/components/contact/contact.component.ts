9bc6582832183b65c578f05af95cee7998e4f0d0
ManuelRetamosaJaenes02 <callofdutymemi@gmail.com>
2025-05-29 09:33:21 +0200

    Apartados 'videos', 'politica de privacidad', 'aviso legal', 'politica de cookies', 'contacto' y 'declaracion de accesibilidad' añadidos

--- pi-manuelRetamosa-frontend/src/app/components/contact/contact.component.ts
+++ pi-manuelRetamosa-frontend/src/app/components/contact/contact.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import { ContactService} from '../../services/contactService';
+import { ContactDTO} from '../../models/contactDTO';
+import {CommonModule} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contact: ContactDTO = { name: '', email: '', message: '' };
+  errorMsg = '';
 
+  constructor(private contactService: ContactService, private router: Router) {}
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) return;
+
+    this.contactService.send(this.contact).subscribe({
+      next: () => {
+        this.router.navigate(['']);
+      },
+      error: () => {
+        this.errorMsg = 'Error enviando el mensaje.';
+      }
+    });
+  }
 }
---
+++ pi-manuelRetamosa-frontend/src/app/services/contactService.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ContactDTO} from '../models/contactDTO';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ContactService {
+  private apiUrl = 'http://localhost:8080/contact';
+
+  constructor(private http: HttpClient) {}
+
+  send(contact: ContactDTO): Observable<{ message: string }> {
+    const url = `${this.apiUrl}/sendMail`;
+    return this.http.post<{ message: string }>(url, contact);
+  }
+}
